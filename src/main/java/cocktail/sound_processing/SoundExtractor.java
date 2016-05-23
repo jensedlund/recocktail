1259c17b7e18ab1f7810f6500757ef7b2b0e4804
retrobot9k <retrobot9000@gmail.com>
2016-05-23 17:04:16 +0200

    added some comments

--- src/main/java/cocktail/sound_processing/SoundExtractor.java
+++ src/main/java/cocktail/sound_processing/SoundExtractor.java
@@ -7,6 +7,7 @@ public interface SoundExtractor {
   /*
   These methods depend on having sox and ffpeg installed on the system!
   MAKE SURE THEY ARE INSTALLED BEFORE USING THIS.
+
    */
   public boolean vidToWav(File inputFile, File outputFile);
   public boolean soundToWav(File inputFile, File outputFile);
