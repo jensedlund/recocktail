1aebca5df37d5e198dacee4cca8e1cfcf015d9cd
retrobot9k <retrobot9000@gmail.com>
2016-05-23 11:17:56 +0200

    added comments

--- src/main/java/cocktail/sound_processing/SoundExtractor.java
+++ src/main/java/cocktail/sound_processing/SoundExtractor.java
@@ -4,6 +4,10 @@ import java.io.File;
 
 public interface SoundExtractor {
 
+  /*
+  These methods depend on having sox and ffpeg installed on the system!
+  MAKE SURE THEY ARE INSTALLED BEFORE USING THIS.
+   */
   public boolean vidToWav(File inputFile, File outputFile);
   public boolean soundToWav(File inputFile, File outputFile);
 
--- src/main/java/cocktail/sound_processing/SoundProcess.java
+++ src/main/java/cocktail/sound_processing/SoundProcess.java
@@ -5,6 +5,9 @@ import java.io.ByteArrayOutputStream;
 
 public interface SoundProcess {
 
+  /*
+  Trims a snippet of sound from a longer audio clip.
+   */
   public byte[] trimAudioClip(byte[] byteArray, double startSecond, double secondsToCopy);
   public double getSnippetLength (byte[] snippet);
 }
